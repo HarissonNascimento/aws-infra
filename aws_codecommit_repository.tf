7bedd870ea03726ac174ce7937c4cd54c3b89407
Harisson <harisson.nas@gmail.com>
2024-04-21 22:35:59 -0300

    fix: description

--- aws_codecommit_repository.tf
+++ aws_codecommit_repository.tf
@@ -1,4 +1,4 @@
 resource "aws_codecommit_repository" "mirrored_aws_project_repository" {
   repository_name = "mirrored-aws-project"
-  description     = "This is the AI Document Based Lambda Repository"
+  description     = "This is the Mirrored AWS Project Repository"
 }
